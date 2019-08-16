d9ad41fd41617113daa36d2d821a263605dff348
Elderen Lee <elderen2@gmail.com>
2019-08-16 17:38:48 -0400

    rebuilding WiFiLit. organizing new components. react nagivation implementation.

--- index.js
+++ index.js
@@ -2,14 +2,8 @@
  * @format
  */
 
-import { AppRegistry, YellowBox } from 'react-native';
-import App from './components/App';
-import { name as appName } from './app.json';
-
-YellowBox.ignoreWarnings([
-  'Warning: componentWillMount is deprecated',
-  'Warning: componentWillReceiveProps is deprecated',
-  'Module RCTImageLoader requires',
-]);
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
